02f85402b5033d33a3e09aa0e5715847cf1a7aae
mmoroz <mmoroz@demandbase.com>
2023-10-09 19:58:42 +0300

    Longest substring without repeating characters

---
+++ src/main/java/person/maksym/preparation/LongestSubstringExRepeatingChars.java
@@ -0,0 +1,35 @@
+package main.java.person.maksym.preparation;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringExRepeatingChars {
+
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0) {
+            return 0;
+        } else {
+            int max = 1;
+            int l = 0;
+            int r = 1;
+
+            Set<Character> used = new HashSet<>();
+            used.add(s.charAt(0));
+
+            while (l <= r && r < s.length()) {
+                if (used.add(s.charAt(r)) || l == r) {
+                    max = Math.max(max, r - l + 1);
+                    ++r;
+                } else {
+                    used.remove(s.charAt(l));
+                    ++l;
+                }
+            }
+            return max;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LongestSubstringExRepeatingChars().lengthOfLongestSubstring("pwwkew"));
+    }
+}
