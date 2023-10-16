c6b4bdda1d04769422247a75365cfbde28ab1da2
mmoroz <mmoroz@demandbase.com>
2023-10-16 17:32:52 +0300

    Zigzag Conversion

---
+++ src/main/java/person/maksym/preparation/ZigzagConversion.java
@@ -0,0 +1,27 @@
+package main.java.person.maksym.preparation;
+
+public class ZigzagConversion {
+
+    public String convert(String s, int numRows) {
+        if (numRows < 2) {
+            return s;
+        }
+        final int cycle = numRows * 2 - 2;
+        StringBuilder builder = new StringBuilder(s.length());
+
+        for (int row = 0; row < numRows; ++row) {
+            for (int step = row; step < s.length(); step += cycle) {
+                builder.append(s.charAt(step));
+                int mid = step + cycle - 2 * row;
+                if (row != 0 && row != numRows - 1 && mid < s.length()) {
+                    builder.append(s.charAt(mid));
+                }
+            }
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ZigzagConversion().convert("PAYPALISHIRING", 4));
+    }
+}
