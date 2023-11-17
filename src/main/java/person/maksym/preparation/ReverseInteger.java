4941dfb7eb9732e7f4a209269f1e4f197f87a986
mmoroz <mmoroz@demandbase.com>
2023-11-17 14:51:51 +0200

    Reverse Integer

---
+++ src/main/java/person/maksym/preparation/ReverseInteger.java
@@ -0,0 +1,26 @@
+package main.java.person.maksym.preparation;
+
+public class ReverseInteger {
+
+    public int reverse(int x) {
+        int number = x;
+        long revers = 0;
+
+        while (number != 0) {
+            if (number != x) {
+                revers *= 10;
+            }
+            revers += (number % 10);
+            number /= 10;
+        }
+        if (revers > Integer.MAX_VALUE || revers < Integer.MIN_VALUE) {
+            return 0;
+        } else {
+            return (int) revers;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ReverseInteger().reverse(-123456));
+    }
+}
