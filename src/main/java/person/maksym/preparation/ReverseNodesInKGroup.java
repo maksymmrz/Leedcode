db5972f4c7c5836236bfc019eb7bd5da7d744faa
mmoroz <mmoroz@demandbase.com>
2023-11-29 17:13:39 +0200

    Reverse Nodes in k-Group

---
+++ src/main/java/person/maksym/preparation/ReverseNodesInKGroup.java
@@ -0,0 +1,78 @@
+package main.java.person.maksym.preparation;
+
+
+public class ReverseNodesInKGroup {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode ln(int val, ListNode node) {
+        return new ListNode(val, node);
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        ListNode start = null;
+        ListNode finish = null;
+
+        while (head != null) {
+            ListNode top = null;
+            ListNode tail = head;
+
+            int remainCount = 0;
+            ListNode pointer = head;
+            while (pointer != null && remainCount < k) {
+                remainCount++;
+                pointer = pointer.next;
+            }
+
+            if (remainCount == k) {
+                for (int i = 0; i < k; i++) {
+                    ListNode curr = head;
+                    head = head.next;
+                    curr.next = top;
+                    top = curr;
+                }
+            } else {
+                top = head;
+                head = head.next;
+            }
+
+            if (start == null) {
+                start = top;
+                finish = tail;
+            } else {
+                finish.next = top;
+                finish = tail;
+            }
+        }
+
+        return start;
+    }
+
+    public static void main(String[] args) {
+        ListNode list = ln(1, null);
+        ListNode tail = list;
+        for (int i = 2; i < 18; i++) {
+            tail.next = ln(i, null);
+            tail = tail.next;
+        }
+        ListNode reversed = new ReverseNodesInKGroup().reverseKGroup(list, 5);
+        while (reversed != null) {
+            System.out.println(reversed.val);
+            reversed = reversed.next;
+        }
+    }
+}
