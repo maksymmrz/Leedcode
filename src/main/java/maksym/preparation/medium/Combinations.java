e34cfa29a4ec4879d77869b9415b5a3a839e9e58
maksymmoroz <maksymmrz@gmail.com>
2024-07-17 17:08:44 +0200

    Combinations

---
+++ src/main/java/maksym/preparation/medium/Combinations.java
@@ -0,0 +1,32 @@
+package maksym.preparation.medium;
+
+import maksym.preparation.util.Util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(new ArrayList<>(), 1, n, k, result);
+        return result;
+    }
+
+    public void backtrack(List<Integer> combination, int curr, int n, int k, List<List<Integer>> result) {
+        if (k == 0) {
+            result.add(new ArrayList<>(combination));
+        } else {
+            for (int i = curr; i <= n - k + 1; i++) {
+                combination.add(i);
+                backtrack(combination, i + 1, n, k - 1, result);
+                combination.remove(combination.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Combinations instance = new Combinations();
+
+        Util.show2dList(instance.combine(4, 2));
+    }
+}
