e598f80975b1d4c891ab4d94ec2ee99929087ce3
maksymmoroz <maksymmrz@gmail.com>
2024-07-21 19:14:45 +0200

    Lowest Common Ancestor of a Binary Tree III

---
+++ src/main/java/maksym/preparation/medium/LowestCommonAncestorBinaryTreeIII.java
@@ -0,0 +1,35 @@
+package maksym.preparation.medium;
+
+import maksym.preparation.util.TreeNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LowestCommonAncestorBinaryTreeIII {
+
+    public TreeNode lowestCommonAncestor(TreeNode p, TreeNode q) {
+        return lowestCommonAncestorRec(p, q, new HashSet<>());
+    }
+
+    public TreeNode lowestCommonAncestorRec(TreeNode a, TreeNode b, Set<TreeNode> used) {
+        if (a != null && !used.add(a)) {
+            return a;
+        }
+        if (b != null && !used.add(b)) {
+            return b;
+        }
+        if (a != null) a = a.parent;
+        if (b != null) b = b.parent;
+        return lowestCommonAncestorRec(a, b, used);
+    }
+
+    public static void main(String[] args) {
+        LowestCommonAncestorBinaryTreeIII instance = new LowestCommonAncestorBinaryTreeIII();
+
+        Integer[] data = new Integer[]{1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16};
+        TreeNode tree = TreeNode.tree(0, data);
+        TreeNode.show(tree);
+        System.out.println(instance.lowestCommonAncestor(tree.left.left.left, tree.left.right.right).val);
+
+    }
+}
--- src/main/java/maksym/preparation/util/TreeNode.java
+++ src/main/java/maksym/preparation/util/TreeNode.java
@@ -1,12 +1,10 @@
 package maksym.preparation.util;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class TreeNode {
     public int val;
     public TreeNode left = null;
     public TreeNode right = null;
+    public TreeNode parent = null;
 
     public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
@@ -21,24 +19,55 @@ public class TreeNode {
     public static TreeNode create(int val, TreeNode left, TreeNode right) {
         return new TreeNode(val, left, right);
     }
+
     public static TreeNode create(int val) {
         return new TreeNode(val);
     }
 
-    public static void show(TreeNode tree) {
-        Queue<TreeNode> queue = new LinkedList<>();
-        if (tree != null) queue.offer(tree);
-
-        while (!queue.isEmpty()) {
-            final int size = queue.size();
-
-            for (int i = 0; i < size; i++) {
-                TreeNode node = queue.poll();
-                if (node.left != null) queue.offer(node.left);
-                if (node.right != null) queue.offer(node.right);
-                System.out.print(node.val + " ");
+    public static TreeNode tree(int i, Integer[] nums) {
+        if (i >= nums.length || nums[i] == null) return null;
+        else {
+            TreeNode left = tree(i * 2 + 1, nums);
+            TreeNode right = tree(i * 2 + 2, nums);
+            TreeNode node = new TreeNode(nums[i], left, right);
+            if (left != null) {
+                left.parent = node;
             }
-            System.out.println();
+            if (right != null) {
+                right.parent = node;
+            }
+            return node;
         }
+    }
+
+    public static void show(TreeNode tree) {
+        int depth = depth(tree);
+        String[] layers = new String[depth];
+        traverse(tree, depth - 1, layers);
+        for (int i = layers.length - 1; i >= 0; i--) {
+            System.out.println(layers[i]);
+        }
+    }
+
+    private static void traverse(TreeNode tree, int i, String[] layers) {
+        if (tree != null || i >= 0) {
+            if (layers[i] == null) layers[i] = "   ".repeat((int) Math.pow(2, i));
+
+            int width = (int) Math.pow(2, i) * 6;
+            String val = tree == null ? "null" : String.valueOf(tree.val);
+            layers[i] += String.format("%-" + width + "s", val);
+            if (tree != null) {
+                traverse(tree.left, i - 1, layers);
+                traverse(tree.right, i - 1, layers);
+            } else {
+                traverse(null, i - 1, layers);
+                traverse(null, i - 1, layers);
+            }
+        }
+    }
+
+    private static int depth(TreeNode tree) {
+        if (tree == null) return 0;
+        else return Math.max(depth(tree.left), depth(tree.right)) + 1;
     }
 }
