535aa7a99c14f8ad911d288792d0e9cd02d67374
maksymmoroz <maksymmrz@gmail.com>
2024-10-02 14:02:10 +0200

    Binary Tree Upside Down

---
+++ src/main/java/maksym/preparation/medium/trees/BinaryTreeUpsideDown.java
@@ -0,0 +1,29 @@
+package maksym.preparation.medium.trees;
+
+import maksym.preparation.util.TreeNode;
+
+import static maksym.preparation.util.TreeNode.create;
+
+public class BinaryTreeUpsideDown {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root != null && root.left != null) {
+            TreeNode down = upsideDownBinaryTree(root.left);
+
+            root.left.right = root;
+            root.left.left = root.right;
+
+            root.left = null;
+            root.right = null;
+
+            return down;
+        }
+        return root;
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeUpsideDown instance = new BinaryTreeUpsideDown();
+        TreeNode tree = create(1, create(2, create(4), create(5)), create(3));
+        TreeNode.show(tree);
+        TreeNode.show(instance.upsideDownBinaryTree(tree));
+    }
+}
