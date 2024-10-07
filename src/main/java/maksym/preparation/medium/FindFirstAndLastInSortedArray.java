1572149313e0af04871615e99ddac84ebae57ced
maksymmoroz <maksymmrz@gmail.com>
2024-10-07 15:49:19 +0200

    [Improvement] Find First and Last Position of Element in Sorted Array

--- src/main/java/maksym/preparation/medium/FindFirstAndLastInSortedArray.java
+++ src/main/java/maksym/preparation/medium/FindFirstAndLastInSortedArray.java
@@ -3,41 +3,35 @@ package maksym.preparation.medium;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class FindFirstAndLastPositionOfElementInSortedArray {
+public class FindFirstAndLastInSortedArray {
 
     public int[] searchRange(int[] nums, int target) {
+        final int n = nums.length;
+        int start;
         int l = 0;
-        int r = nums.length - 1;
+        int r = n - 1;
 
         while (l < r) {
-            int mid = (l + r + 1) / 2;
-            if (target < nums[mid]) {
-                r = mid - 1;
-            } else {
-                l = mid;
-            }
+            int m = (l + r) / 2;
+            if (nums[m] < target) l = m + 1;
+            else r = m;
         }
-        int right = l;
-        l = 0;
-        r = nums.length - 1;
-
+        start = l;
+        if (n == 0 || nums[start] != target) return new int[]{-1, -1};
+        r = n - 1;
         while (l < r) {
-            int mid = (l + r) / 2;
-            if (nums[mid] < target) {
-                l = mid + 1;
-            } else {
-                r = mid;
-            }
+            int m = (l + r + 1) / 2;
+            if (nums[m] <= target) l = m;
+            else r = m - 1;
         }
-        if (nums.length <= l || nums[l] != target) return new int[]{-1, - 1};
-        else return new int[]{l, right};
+        return new int[]{start, r};
     }
 
     public static void main(String[] args) {
         int[] arr1 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 10, 11, 12, 13};
         int[] arr2 = new int[]{1, 2, 3, 4, 4, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         int[] arr3 = new int[]{5, 7, 7, 9, 9, 10};
-        FindFirstAndLastPositionOfElementInSortedArray instance = new FindFirstAndLastPositionOfElementInSortedArray();
+        FindFirstAndLastInSortedArray instance = new FindFirstAndLastInSortedArray();
         System.out.println(Arrays.stream(instance.searchRange(arr1, 8)).boxed().collect(Collectors.toList()));
         System.out.println(Arrays.stream(instance.searchRange(arr2, 4)).boxed().collect(Collectors.toList()));
         System.out.println(Arrays.stream(instance.searchRange(arr3, 7)).boxed().collect(Collectors.toList()));
