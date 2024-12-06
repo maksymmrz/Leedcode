7b29abb57635dd214721b0a236a59a310cfc3b73
maksymmoroz <maksymmrz@gmail.com>
2024-12-06 14:59:27 +0100

    Decode Ways

--- src/main/java/maksym/preparation/medium/DecodeWays.java
+++ src/main/java/maksym/preparation/medium/DecodeWays.java
@@ -4,24 +4,35 @@ package maksym.preparation.medium;
 public class DecodeWays {
 
     public int numDecodings(String s) {
-        if (s.startsWith("0")) return 0;
-        int[] dp = new int[s.length() + 1];
-        dp[0] = 1;
-        dp[1] = 1;
+        if (s.isEmpty()) return 1;
+        if (s.charAt(0) == '0') return 0;
+
+        int curr = 1;
+        int prev = 1;
 
         for (int i = 1; i < s.length(); i++) {
-            int first = s.charAt(i - 1) - '0';
-            int second = s.charAt(i) - '0';
-            int two = first * 10 + second;
-            if (two >= 10 && two <= 26) dp[i + 1] += dp[i - 1];
-            if (second != 0) dp[i + 1] += dp[i];
+            if (s.charAt(i) == '0') {
+                if (s.charAt(i - 1) == '1' || s.charAt(i - 1) == '2') {
+                    int tmp = curr;
+                    curr = prev;
+                    prev = tmp;
+                } else {
+                    return 0;
+                }
+            } else if ((s.charAt(i - 1) == '1') || (s.charAt(i - 1) == '2' && s.charAt(i) <= '6')) {
+                int ways = curr + prev;
+                prev = curr;
+                curr = ways;
+            } else {
+                prev = curr;
+            }
         }
-        return dp[s.length()];
+        return curr;
     }
 
     public static void main(String[] args) {
         DecodeWays instance = new DecodeWays();
         System.out.println(instance.numDecodings("120"));
-        System.out.println(instance.numDecodings("226"));
+        System.out.println(instance.numDecodings("1201234"));
     }
 }
