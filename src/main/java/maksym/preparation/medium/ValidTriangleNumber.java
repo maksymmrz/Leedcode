4e1cad3102a6e62901e063c1f79d6810d40a9f6d
maksymmoroz <maksymmrz@gmail.com>
2024-12-12 12:35:17 +0100

    Valid Triangle Number

--- src/main/java/maksym/preparation/medium/ValidTriangleNumber.java
+++ src/main/java/maksym/preparation/medium/ValidTriangleNumber.java
@@ -7,28 +7,25 @@ public class ValidTriangleNumber {
         Arrays.sort(nums);
         int count = 0;
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            int k = i + 1;
-            for (int j = i + 1; j < nums.length - 1; j++) {
-                k = Math.max(j, k);
+        for (int i = nums.length - 1; i >= 2; i--) {
+            int l = 0;
+            int r = i - 1;
 
-                while (k + 1 < nums.length && isValid(nums[i], nums[j], nums[k + 1])) k++;
-                count += k - j;
+            while (l < r) {
+                if (nums[l] + nums[r] <= nums[i]) {
+                    l++;
+                } else {
+                    count += r - l;
+                    r--;
+                }
             }
         }
         return count;
     }
 
-    private static boolean isValid(int first, int second, int third) {
-        return (first + second) > third &&
-                (first + third) > second &&
-                (second + third) > first;
-    }
-
     public static void main(String[] args) {
         ValidTriangleNumber o = new ValidTriangleNumber();
-        System.out.println(o.triangleNumber(new int[]{1,1,3,4}));
-        System.out.println(o.triangleNumber(new int[]{2,2,3,4}));
+        System.out.println(o.triangleNumber(new int[]{2, 2, 3, 4}));
         System.out.println(o.triangleNumber(new int[]{4, 2, 3, 4}));
     }
 }
