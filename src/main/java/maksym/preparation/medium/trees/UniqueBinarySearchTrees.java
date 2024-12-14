5d27e1971500921502617d67449dcae1978a7dac
maksymmoroz <maksymmrz@gmail.com>
2024-12-14 21:16:43 +0100

    Unique Binary Search Trees

--- src/main/java/maksym/preparation/medium/trees/UniqueBinarySearchTrees.java
+++ src/main/java/maksym/preparation/medium/trees/UniqueBinarySearchTrees.java
@@ -3,16 +3,14 @@ package maksym.preparation.medium.trees;
 public class UniqueBinarySearchTrees {
     public int numTrees(int n) {
         int[] dp = new int[n + 1];
-        dp[0] = 1;
+        dp[0] = dp[1] = 1;
 
-        for (int len = 1; len <= n; len++) {
-
-            for (int i = 0; i <= len - 1; i++) {
-                int left = dp[len - 1 - i];
-                int right = dp[i];
-                dp[len] += left * right;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
             }
         }
+
         return dp[n];
     }
 
