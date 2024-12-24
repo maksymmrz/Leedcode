649a148966ddd20ea915ccf28cc345f321c6ed24
maksymmoroz <maksymmrz@gmail.com>
2024-12-24 21:35:56 +0100

    Asteroid Collision

---
+++ src/main/java/maksym/preparation/medium/AsteroidCollision.java
@@ -0,0 +1,34 @@
+package maksym.preparation.medium;
+
+import maksym.preparation.util.Util;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class AsteroidCollision {
+    public int[] asteroidCollision(int[] asteroids) {
+        Deque<Integer> st = new LinkedList<>();
+
+        for (int a : asteroids) {
+            while (!st.isEmpty() && st.peek() > 0 && st.peek() < -a) st.pop();
+            if (!st.isEmpty() && st.peek() > 0 && st.peek() == -a) {
+                st.pop();
+            } else if (st.isEmpty() || a > 0 || st.peek() < 0) {
+                st.push(a);
+            }
+        }
+
+        int[] res = new int[st.size()];
+        for (int i = 0; i < res.length && !st.isEmpty(); i++) {
+            res[i] = st.pollLast();
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        AsteroidCollision o = new AsteroidCollision();
+        Util.show(o.asteroidCollision(new int[]{5, 10, -5}));
+        Util.show(o.asteroidCollision(new int[]{15, 5, 10, -11, 1}));
+    }
+}
