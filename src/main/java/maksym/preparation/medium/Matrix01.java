d351dd1520963644220cb261a3b765ede6cc6f0d
maksymmoroz <maksymmrz@gmail.com>
2025-01-16 20:35:03 +0100

    01 Matrix

--- src/main/java/maksym/preparation/medium/Matrix01.java
+++ src/main/java/maksym/preparation/medium/Matrix01.java
@@ -2,52 +2,40 @@ package maksym.preparation.medium;
 
 import maksym.preparation.util.Util;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 public class Matrix01 {
     public int[][] updateMatrix(int[][] mat) {
         final int m = mat.length;
         final int n = mat[0].length;
 
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] visited = new boolean[m][n];
+        int[][] dp = new int[m][n];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
-                    q.add(new int[]{i, j});
-                    visited[i][j] = true;
+                    dp[i][j] = 0;
+                } else {
+                    int left = j > 0 ? dp[i][j - 1] : m * n;
+                    int upper = i > 0 ? dp[i - 1][j] : m * n;
+
+                    dp[i][j] = 1 + Math.min(left, upper);
                 }
+
             }
         }
-        int count = 0;
-        int[][] dirs = new int[][]{{0, 1}, {1, 0}, {-1, 0}, {0, -1}};
-        int[][] rm = new int[m][n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (mat[i][j] == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    int right = j < n - 1 ? dp[i][j + 1] : m * n;
+                    int down = i < m - 1 ? dp[i + 1][j] : m * n;
 
-        while (!q.isEmpty()) {
-            int size = q.size();
-
-            for (int c = 0; c < size; c++) {
-                int[] p = q.poll();
-                assert p != null;
-                int i = p[0];
-                int j = p[1];
-
-                rm[i][j] = count;
-
-                for (int[] dir : dirs) {
-                    int x = i + dir[0];
-                    int y = j + dir[1];
-                    if (x >= 0 && x < m && y >= 0 && y < n && !visited[x][y]) {
-                        visited[x][y] = true;
-                        q.add(new int[]{x, y});
-                    }
+                    dp[i][j] = Math.min(Math.min(right, down) + 1, dp[i][j]);
                 }
+
             }
-            count++;
         }
-        return rm;
+        return dp;
     }
 
     public static void main(String[] args) {
