ac93176fc9d169c4bd4691f67820cad5bbc64667
maksymmoroz <maksymmrz@gmail.com>
2025-07-29 11:23:44 +0200

    [Update] Longest Common Prefix

--- src/main/java/maksym/preparation/easy/LongestCommonPrefix.java
+++ src/main/java/maksym/preparation/easy/LongestCommonPrefix.java
@@ -1,22 +1,34 @@
 package maksym.preparation.easy;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 public class LongestCommonPrefix {
     public String longestCommonPrefix(String[] strs) {
-        Arrays.sort(strs);
-        String left = strs[0];
-        String right = strs[strs.length - 1];
-        int limit = 0;
-        final int min = Math.min(left.length(), right.length());
+        int pos = 0;
+        int len = strs[0].length();
 
-        while (limit < min && left.charAt(limit) == right.charAt(limit)) limit++;
+        for (String str : strs) {
+            len = Math.min(len, str.length());
+        }
 
-        return limit == 0 ? "" : left.substring(0, limit);
+        for (; pos < len; pos++) {
+            char ch = strs[0].charAt(pos);
+            boolean notSame = false;
+
+            for (var s : strs) {
+                if (s.charAt(pos) != ch) {
+                    notSame = true;
+                    break;
+                }
+            }
+
+            if (notSame) {
+                break;
+            }
+        }
+
+        return strs[0].substring(0, pos);
     }
 
     public static void main(String[] args) {
-        System.out.println(new LongestCommonPrefix().longestCommonPrefix(new String[] {"flower","flow","flight"}));
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
     }
 }
