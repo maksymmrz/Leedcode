d1c3daa3cef7b3228cc3648319efec35767e6b25
maksymmoroz <maksymmrz@gmail.com>
2025-07-30 15:18:25 +0200

    [Update] - Perfect Squares

--- src/main/java/maksym/preparation/medium/PerfectSquares.java
+++ src/main/java/maksym/preparation/medium/PerfectSquares.java
@@ -1,21 +1,36 @@
 package maksym.preparation.medium;
 
 public class PerfectSquares {
-    public int numSquares(int n) {
-        final int sqrtMax = 1 + (int)Math.sqrt(n);
-        int[] dp = new int[n + 1];
 
-        for (int i = 1; i <= n; i++) {
-            dp[i] = Integer.MAX_VALUE;
-            for (int j = 1; j <= sqrtMax && j * j <= i; j++) {
-                dp[i] = Math.min(dp[i - j * j] + 1, dp[i]);
+    public int numSquares(int n) {
+        for (int c = 1; c <= n; c++) {
+            if (isDivided(n, c)) {
+                return c;
             }
         }
-        return dp[n];
+
+        return n;
+    }
+
+    private boolean isDivided(int n, int count) {
+        int sqrt = (int) Math.sqrt(n);
+
+        if (count == 1) {
+            return sqrt * sqrt == n;
+        }
+
+        for (int i = 1; i <= sqrt; i++) {
+            int m = i * i;
+
+            if (isDivided(n - m, count - 1)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         PerfectSquares o = new PerfectSquares();
-        System.out.println(o.numSquares(12));
+        System.out.println(o.numSquares(13));
     }
 }
