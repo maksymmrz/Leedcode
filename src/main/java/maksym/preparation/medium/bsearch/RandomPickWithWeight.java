ee835d99c07ad2b120dc4690b75e2872981dca34
maksymmoroz <maksymmrz@gmail.com>
2025-08-04 12:52:51 +0200

    [Update] - Random Pick with Weight

--- src/main/java/maksym/preparation/medium/bsearch/RandomPickWithWeight.java
+++ src/main/java/maksym/preparation/medium/bsearch/RandomPickWithWeight.java
@@ -4,36 +4,43 @@ import java.util.Random;
 
 public class RandomPickWithWeight {
     private final Random rand;
+    private final int lim;
     private final int[] sums;
-    private final int max;
 
     public RandomPickWithWeight(int[] w) {
-        rand = new Random(System.currentTimeMillis());
-        sums = new int[w.length];
+        this.rand = new Random(System.currentTimeMillis());
+        this.sums = new int[w.length];
 
+        int prev = 0;
         for (int i = 0; i < w.length; i++) {
-            sums[i] = w[i];
-            if (i > 0) sums[i] += sums[i - 1];
+            this.sums[i] = prev + w[i];
+            prev = this.sums[i];
         }
-        max = sums[sums.length - 1];
+
+        this.lim = prev;
     }
 
     public int pickIndex() {
-        int num = 1 + rand.nextInt(max);
+        int n = rand.nextInt(lim) + 1;
 
         int l = 0;
         int r = sums.length - 1;
+
         while (l < r) {
             int m = (l + r) / 2;
-            if (sums[m] < num) l = m + 1;
-            else r = m;
+            if (sums[m] < n) {
+                l = m + 1;
+            } else {
+                r = m;
+            }
         }
+
         return l;
     }
 
     public static void main(String[] args) {
-        RandomPickWithWeight r = new RandomPickWithWeight(new int[] {4, 3, 1, 1, 1, 0});
-        int[] counter = new int[6];
+        RandomPickWithWeight r = new RandomPickWithWeight(new int[] {4, 3, 2, 1});
+        int[] counter = new int[4];
         for (int i = 0; i < 10000; i++) {
             counter[r.pickIndex()]++;
         }
