7b2b518fc836ea145fa871a758c9932de24130e1
maksymmoroz <maksymmrz@gmail.com>
2025-12-21 18:09:05 +0100

    Median of Two Sorted Arrays [Update]

--- src/main/java/maksym/preparation/hard/bsearch/MedianTwoSortedArrays.java
+++ src/main/java/maksym/preparation/hard/bsearch/MedianTwoSortedArrays.java
@@ -5,55 +5,69 @@ public class MedianTwoSortedArrays {
         if (nums1.length < nums2.length) {
             return findMedianSortedArrays(nums2, nums1);
         }
-        final int m = nums1.length;
-        final int n = nums2.length;
-        final int len = m + n;
-        final int half = (len + 1) / 2;
+
+        final int M = nums1.length;
+        final int N = nums2.length;
+
+        if (N == 0) {
+            int med = M / 2;
+            return M % 2 == 0 ? (nums1[med - 1] + nums1[med]) / 2.0 : nums1[med];
+        }
+
+        final int half = (M + N - 1) / 2;
 
         int l = 0;
-        int r = n - 1;
-
-        if (n == 0) {
-            int med = m / 2;
-            return m % 2 == 0 ? (nums1[med - 1] + nums1[med]) / 2.0 : nums1[med];
-        }
+        int r = N - 1;
 
         while (l < r) {
-            int med = (l + r) / 2;
-            int i = half - med - 1;
-            if (nums2[med] < nums1[i]) l = l + 1;
-            else r = med;
-        }
-        final int firstMed = half - l - 1;
-        final int secondMed = l;
+            int j = l + (r - l) / 2;
+            int i = half - j;
 
-        if (len % 2 == 0) {
-            if (nums1[firstMed] <= nums2[secondMed]) {
-                int f1 = nums1[firstMed];
-                int f2 = firstMed + 1 < m ? nums1[firstMed + 1] : Integer.MAX_VALUE;
-                int s1 = secondMed > 0 ? nums2[secondMed - 1] : Integer.MIN_VALUE;
-                int s2 = nums2[secondMed];
-                return (Math.max(f1, s1) + Math.min(f2, s2)) / 2.0;
+            if (nums1[i] > nums2[j]) {
+                l = j + 1;
             } else {
-                int f1 = firstMed > 0 ? nums1[firstMed - 1] : Integer.MIN_VALUE;
-                int f2 = nums1[firstMed];
-                int s = nums2[secondMed];
-                return (Math.max(f1, s) + f2) / 2.0;
+                r = j;
+            }
+        }
+
+        int i = half - l;
+        int j = l;
+
+        if ((M + N) % 2 == 0) {
+            if (nums1[i] <= nums2[j]) {
+                int maxFirst = nums1[i];
+                int maxSecond = j > 0 ? nums2[j - 1] : Integer.MIN_VALUE;
+
+                int minFirst = i < M - 1 ? nums1[i + 1] : Integer.MAX_VALUE;
+                int minSecond = nums2[j];
+
+                return ((double) Math.max(maxFirst, maxSecond) + Math.min(minFirst, minSecond)) / 2;
+            } else {
+                int n1 = i > 0 ? nums1[i - 1] : Integer.MIN_VALUE;
+                int n2 = nums2[j];
+
+                int n3 = nums1[i];
+                int n4 = j < N - 1 ? nums2[j + 1] : Integer.MAX_VALUE;
+
+                return ((double) Math.max(n1, n2) + Math.min(n3, n4)) / 2;
             }
         } else {
-            if (nums1[firstMed] < nums2[secondMed]) {
-                int f = nums1[firstMed];
-                int s = secondMed > 0 ? nums2[secondMed - 1] : Integer.MIN_VALUE;
-                return Math.max(f, s);
+            if (nums1[i] <= nums2[j]) {
+                int n1 = nums1[i];
+                int n2 = j > 0 ? nums2[j - 1] : Integer.MIN_VALUE;
+                return Math.max(n1, n2);
             } else {
-                return Math.max(nums1[firstMed - 1], nums2[secondMed]);
+                int n1 = i > 0 ? nums1[i - 1] : Integer.MIN_VALUE;
+                int n2 = nums2[j];
+                return Math.max(n1, n2);
             }
         }
     }
 
     public static void main(String[] args) {
-        int[] arr1 = new int[]{1,2,3,4,5};
-        int[] arr2 = new int[]{6,7,8,9,11,12,13,14,15,16,17};
+        int[] arr1 = new int[]{1, 3, 5, 7};
+        int[] arr2 = new int[]{2, 4, 6};
+
         System.out.println(findMedianSortedArrays(arr1, arr2));
     }
 }
