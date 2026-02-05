a11eb6f5fd7a52339f664fc47e10c846d2f4b55f
maksymmoroz <maksymmrz@gmail.com>
2026-02-05 13:28:45 +0100

    Regular Expression Matching [Update]

--- src/main/java/maksym/preparation/hard/dp/RegularExpressionMatching.java
+++ src/main/java/maksym/preparation/hard/dp/RegularExpressionMatching.java
@@ -3,33 +3,46 @@ package maksym.preparation.hard.dp;
 public class RegularExpressionMatching {
 
     public boolean isMatch(String s, String p) {
-        final int n = s.length() + 1;
-        final int m = p.length() + 1;
-        boolean[][] matrix = new boolean[n][m];
+        final var M = s.length() + 1;
+        final var N = p.length() + 1;
 
-        matrix[0][0] = true;
-        for (int j = 0; j < m - 1; ++j) {
-            matrix[0][j + 1] = p.charAt(j) == '*' && matrix[0][j - 1];
+        var dp = new boolean[M][N];
+        dp[0][0] = true;
+        for (int j = 1; j < N; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
         }
 
-        for(int i = 1; i < n; ++i) {
-            for (int j = 1; j < m; ++j) {
-                if (p.charAt(j - 1) == '*') {
-                    matrix[i][j] =
-                            ((s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.') &&
-                                    (matrix[i][j - 1] || matrix[i - 1][j - 1] || matrix[i - 1][j]))
-                            || matrix[i][j - 2];
-                } else if (p.charAt(j - 1) == '.') {
-                    matrix[i][j] = matrix[i - 1][j - 1];
+        for (int i = 1; i < M; i++) {
+            for (int j = 1; j < N; j++) {
+                var x = i - 1;
+                var y = j - 1;
+
+                if (p.charAt(y) == '.') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(y) == '*') {
+
+                    if (p.charAt(y - 1) == '.') {
+                        dp[i][j] = dp[i - 1][j] || dp[i][j - 2];
+                    } else {
+                        if (s.charAt(x) == p.charAt(y - 1)) {
+                            dp[i][j] = dp[i - 1][j] || dp[i][j - 2];
+                        } else {
+                            dp[i][j] = dp[i][j - 2];
+                        }
+                    }
                 } else {
-                    matrix[i][j] = s.charAt(i - 1) == p.charAt(j - 1) && matrix[i - 1][j - 1];
+                    dp[i][j] = s.charAt(x) == p.charAt(y) && dp[i - 1][j - 1];
                 }
             }
         }
-        return matrix[n - 1][m - 1];
+
+        return dp[M - 1][N - 1];
     }
 
     public static void main(String[] args) {
+        System.out.println(new RegularExpressionMatching().isMatch("aaa", "ab*a*c*a"));
         System.out.println(new RegularExpressionMatching().isMatch("a", ".*."));
     }
 }
