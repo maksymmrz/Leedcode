058340dff7e5e8ed3808b467ac63c1124d53749f
maksymmoroz <maksymmrz@gmail.com>
2026-02-17 10:19:44 +0100

    Course Schedule [Update]

--- src/main/java/maksym/preparation/medium/graph/CourseSchedule.java
+++ src/main/java/maksym/preparation/medium/graph/CourseSchedule.java
@@ -4,49 +4,42 @@ import java.util.*;
 
 public class CourseSchedule {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        var edges = new HashMap<Integer, List<Integer>>();
+        var adj = new HashMap<Integer, List<Integer>>();
+        var indegree = new int[numCourses];
 
         for (int[] edge : prerequisites) {
-            final var start = edge[1];
-            final var end = edge[0];
-            edges.computeIfAbsent(start, k -> new ArrayList<>()).add(end);
+            var from = edge[1];
+            var to = edge[0];
+            adj.computeIfAbsent(from, k -> new ArrayList<>()).add(to);
+            indegree[to]++;
         }
 
-        var visited = new boolean[numCourses];
+        var q = new LinkedList<Integer>();
 
-        for (int node = 0; node < numCourses; node++) {
-            var noLoop = detectLoop(node, edges, visited, new boolean[numCourses]);
-
-            if (!noLoop) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean detectLoop(int node, HashMap<Integer, List<Integer>> edges, boolean[] visited, boolean[] branchVisited) {
-        if (branchVisited[node]) {
-            return false;
-        }
-        if (visited[node]) {
-            return true;
-        }
-
-        visited[node] = true;
-        branchVisited[node] = true;
-
-        var refs = edges.getOrDefault(node, Collections.emptyList());
-        for (var next : refs) {
-            var noLoop = detectLoop(next, edges, visited, branchVisited);
-
-            if (!noLoop) {
-                return false;
+        for (int n = 0; n < numCourses; n++) {
+            if (indegree[n] == 0) {
+                q.add(n);
             }
         }
 
-        branchVisited[node] = false;
+        var orderedCount = 0;
 
-        return true;
+        while (!q.isEmpty()) {
+            orderedCount++;
+
+            var n = q.poll();
+            var descendants = adj.getOrDefault(n, Collections.emptyList());
+
+            for (var descendant : descendants) {
+                indegree[descendant]--;
+
+                if (indegree[descendant] == 0) {
+                    q.add(descendant);
+                }
+            }
+        }
+
+        return orderedCount == numCourses;
     }
 
     public static void main(String[] args) {
